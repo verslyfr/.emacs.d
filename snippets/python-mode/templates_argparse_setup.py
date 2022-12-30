43635de7c0a1c386b05dbbc0d8a051396f991eb4
FR Lyvers <verslyfr@github.com>
2022-12-30 09:29:33 -0500

    Add docstring support.

--- snippets/python-mode/templates_argparse_setup.py
+++ snippets/python-mode/templates_argparse_setup.py
@@ -3,10 +3,21 @@
 # key: argparse_setup>
 # group: templates
 # --
+## the following should be moved to the top of the file. The Version number should be adjusted
+## the __VERSION represents "Major.Minor.Bug"
 import argparse
 __VERSION= "0.0.1"
 
-def getArgs(argv=None):
+def get_args(argv=None):
+    """Parses the command-line arguments.
+
+    Args:
+        argv (t.Any): (default: None) A list of arguments.
+          If None then _sys.argv[1:] is used as the input. Supports testing of inputs via scripts.
+    
+    Returns:
+        the results of calling argparse.ArgumentParser.parse_args
+    """
     parser: argparse.ArgumentParser = argparse.ArgumentParser(description="${1:description of the script}")
     parser.add_argument(
         "--version",action="version",version=__VERSION
@@ -21,4 +32,11 @@ def getArgs(argv=None):
         "verbose logging . CRITICAL=0, ERROR=1, WARN=2, INFO=3, "
         "DEBUG=4)",
     )
-    return parser.parse_args(argv)
+
+    args = parser.parse_args(argv)
+
+    # Place any checks to perform on the args here. For example, if using a string for filename,
+    # check for the existance here. parser.error and parser.exit can be used to share specific
+    # issues out of this routine and stop execution.
+    
+    return args
