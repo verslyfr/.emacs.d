59e509f62a059e610716e29fac9305c1de03e8ff
FR Lyvers <verslyfr@github.com>
2023-06-22 19:00:49 -0400

    Trying to change modifications to compare buffers instead.

--- init.el
+++ init.el
@@ -1490,6 +1490,37 @@ R1 and R2 define the selected region."
 ;; (setenv "SHELL" shell-file-name)
 ;; (add-hook 'comint-output-filter-functions 'comint-strip-ctrl-m)
 
+;; Ignore modification-time-only changes in files, i.e. ones that
+;; don't really change the contents.  This happens often with
+;; switching between different VC buffers.
+
+(defun update-buffer-modtime-if-byte-identical ()
+  (let* ((size      (buffer-size))
+         (byte-size (position-bytes size))
+         (filename  buffer-file-name))
+    (when (and byte-size (<= size 1000000))
+      (let* ((attributes (file-attributes filename))
+             (file-size  (nth 7 attributes)))
+        (when (and file-size
+                   (= file-size byte-size)
+                   (string= (buffer-substring-no-properties 1 (1+ size))
+                            (with-temp-buffer
+                              (insert-file-contents filename)
+                              (buffer-string))))
+          (set-visited-file-modtime (nth 5 attributes))
+          t)))))
+
+(defun verify-visited-file-modtime--ignore-byte-identical (original &optional buffer)
+  (or (funcall original buffer)
+      (with-current-buffer buffer
+        (update-buffer-modtime-if-byte-identical))))
+(advice-add 'verify-visited-file-modtime :around #'verify-visited-file-modtime--ignore-byte-identical)
+
+(defun ask-user-about-supersession-threat--ignore-byte-identical (original &rest arguments)
+  (unless (update-buffer-modtime-if-byte-identical)
+    (apply original arguments)))
+(advice-add 'ask-user-about-supersession-threat :around #'ask-user-about-supersession-threat--ignore-byte-identical)
+
 ;;; custom-file
 ;; Move customization variables to a separate file and load it
 (setq custom-file (locate-user-emacs-file (expand-file-name "custom-vars.el" user-emacs-directory)))
