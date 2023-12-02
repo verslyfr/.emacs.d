9769de97818a50c8ae69456890da082366099dfc
FR Lyvers <verslyfr@github.com>
2023-12-02 04:50:19 -0500

    Close #9. Add clipping tool.

--- init.el
+++ init.el
@@ -1188,6 +1188,24 @@ PRIORITY may be one of the characters ?A, ?B, or ?C."
 ;; (add-hook 'org-mode-hook 'variable-pitch-mode)
 
 
+;;* org capture screen shot
+(defun my-org-screenshot ()
+  "Take a screenshot into a time stamped unique-named file in the
+same directory as the org-buffer and insert a link to this file."
+  (interactive)
+  (setq filename
+        (concat
+         (make-temp-name
+          (concat (buffer-file-name)
+                  "_"
+                  (format-time-string "%Y%m%d_%H%M%S_")) ) ".png"))
+  (shell-command "snippingtool /clip")
+  (shell-command (concat "powershell -command \"Add-Type -AssemblyName System.Windows.Forms;if ($([System.Windows.Forms.Clipboard]::ContainsImage())) {$image = [System.Windows.Forms.Clipboard]::GetImage();[System.Drawing.Bitmap]$image.Save('" filename "',[System.Drawing.Imaging.ImageFormat]::Png); Write-Output 'clipboard content saved as file'} else {Write-Output 'clipboard does not contain image data'}\""))
+  (insert (concat "[[file:" filename "]]"))
+  (org-display-inline-images))
+
+(global-set-key "\C-cs" 'my-org-screenshot)
+
 ;;* org onenote link
 (add-hook 'org-mode-hook #'(lambda () (org-add-link-type "onenote" 'org-onenote-open)))
 (defun org-onenote-open (link)
