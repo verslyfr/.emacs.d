e0bf57fd8c5322ace98e13df44bad2fc14027153
FR Lyvers <verslyfr@github.com>
2024-10-08 04:35:35 -0400

    Add wsl support to getting image from clipboard into a org file.

--- init.el
+++ init.el
@@ -1220,22 +1220,48 @@ PRIORITY may be one of the characters ?A, ?B, or ?C."
 ;;   )
 ;; (add-hook 'org-mode-hook 'variable-pitch-mode)
 
-
 ;;* org capture screen shot
 (defun my-org-screenshot ()
   "Take a screenshot into a time stamped unique-named file in the
 same directory as the org-buffer and insert a link to this file."
   (interactive)
-  (setq filename
-        (concat
-         (make-temp-name
-          (concat (buffer-file-name)
-                  "_"
-                  (format-time-string "%Y%m%d_%H%M%S_")) ) ".png"))
-  (shell-command "snippingtool /clip")
-  (shell-command (concat "powershell -command \"Add-Type -AssemblyName System.Windows.Forms;if ($([System.Windows.Forms.Clipboard]::ContainsImage())) {$image = [System.Windows.Forms.Clipboard]::GetImage();[System.Drawing.Bitmap]$image.Save('" filename "',[System.Drawing.Imaging.ImageFormat]::Png); Write-Output 'clipboard content saved as file'} else {Write-Output 'clipboard does not contain image data'}\""))
-  (insert (concat "[[file:" filename "]]"))
+  (cond
+   ((eq system-type 'windows-nt)
+   (let* ((filename (concat (make-temp-name (concat (buffer-file-name) "_"
+                                                    (format-time-string "%Y%m%d_%H%M%S_")) ) ".png")))
+     (shell-command "snippingtool /clip")
+     (shell-command (concat "powershell.exe -command \"Add-Type -AssemblyName System.Windows.Forms;if ($([System.Windows.Forms.Clipboard]::ContainsImage())) {$image = [System.Windows.Forms.Clipboard]::GetImage();[System.Drawing.Bitmap]$image.Save('" filename "',[System.Drawing.Imaging.ImageFormat]::Png); Write-Output 'clipboard content saved as file'} else {Write-Output 'clipboard does not contain image data'}\""))
+     )
+   (insert (concat "[[file:" filename "]]")))
+   ((string-match "WSL2" operating-system-release)
+    (let* ((filename (concat (make-temp-name (concat (buffer-file-name) "_"
+                                                    (format-time-string "%Y%m%d_%H%M%S_")) ) ".png"))
+           (wslfilename (string-trim (shell-command-to-string (concat "wslpath -m \"$(realpath '" filename "')\""))))
+           (wslshortfile (format "./%s" (file-name-nondirectory wslfilename))))
+           ; (message (format "1|filename=%s|fullpath=%s|shortname=%s" filename wslfilename wslshortfile))
+           ; (shell-command "snippingtool.exe /clip")
+           (shell-command (concat "powershell.exe -command 'Add-Type -AssemblyName System.Windows.Forms;if ($([System.Windows.Forms.Clipboard]::ContainsImage())) {$image = [System.Windows.Forms.Clipboard]::GetImage();[System.Drawing.Bitmap]$image.Save(\""wslfilename "\",[System.Drawing.Imaging.ImageFormat]::Png); Write-Output \"clipboard content saved as file\"} else {Write-Output \"clipboard does not contain image data\"}'"))       
+           (insert (concat "[[file:"wslshortfile"]]"))
+      )
+    )
+   )
   (org-display-inline-images))
+
+;; ;;* org capture screen shot
+;; (defun my-org-screenshot ()
+;;   "Take a screenshot into a time stamped unique-named file in the
+;; same directory as the org-buffer and insert a link to this file."
+;;   (interactive)
+;;   (setq filename
+;;         (concat
+;;          (make-temp-name
+;;           (concat (buffer-file-name)
+;;                   "_"
+;;                   (format-time-string "%Y%m%d_%H%M%S_")) ) ".png"))
+;;   (shell-command "snippingtool /clip")
+;;   (shell-command (concat "powershell -command \"Add-Type -AssemblyName System.Windows.Forms;if ($([System.Windows.Forms.Clipboard]::ContainsImage())) {$image = [System.Windows.Forms.Clipboard]::GetImage();[System.Drawing.Bitmap]$image.Save('" filename "',[System.Drawing.Imaging.ImageFormat]::Png); Write-Output 'clipboard content saved as file'} else {Write-Output 'clipboard does not contain image data'}\""))
+;;   (insert (concat "[[file:" filename "]]"))
+;;   (org-display-inline-images))
 
 (global-set-key "\C-cs" 'my-org-screenshot)
 
--- plugins/org-capture-wsl-image/org-capture-wsl-image.el
+++ plugins/org-capture-wsl-image/org-capture-wsl-image.el
@@ -1,15 +1,33 @@
+;;* org capture screen shot
 (defun my-org-screenshot ()
   "Take a screenshot into a time stamped unique-named file in the
 same directory as the org-buffer and insert a link to this file."
   (interactive)
-  (setq filename
-        (concat
-         (make-temp-name
-          (concat (shell-command (concat "wslpath -m " buffer-file-name))
-                  "_"
-                  (format-time-string "%Y%m%d_%H%M%S_")) ) ".png"))
-  (shell-command "snippingtool.exe /clip")
-  (shell-command (concat "powershell.exe -command \"Add-Type -AssemblyName System.Windows.Forms;if ($([System.Windows.Forms.Clipboard]::ContainsImage())) {$image = [System.Windows.Forms.Clipboard]::GetImage();[System.Drawing.Bitmap]$image.Save('" filename "',[System.Drawing.Imaging.ImageFormat]::Png); Write-Output 'clipboard content saved as file'} else {Write-Output 'clipboard does not contain image data'}\""))
-  (insert (concat "[[file:" filename "]]"))
+  (cond
+   ((eq system-type 'windows-nt)
+   (let* ((filename (concat (make-temp-name (concat (buffer-file-name) "_"
+                                                    (format-time-string "%Y%m%d_%H%M%S_")) ) ".png")))
+     (shell-command "snippingtool /clip")
+     (shell-command (concat "powershell.exe -command \"Add-Type -AssemblyName System.Windows.Forms;if ($([System.Windows.Forms.Clipboard]::ContainsImage())) {$image = [System.Windows.Forms.Clipboard]::GetImage();[System.Drawing.Bitmap]$image.Save('" filename "',[System.Drawing.Imaging.ImageFormat]::Png); Write-Output 'clipboard content saved as file'} else {Write-Output 'clipboard does not contain image data'}\""))
+     )
+   (insert (concat "[[file:" filename "]]")))
+   ((string-match "WSL2" operating-system-release)
+    (let* ((filename (concat (make-temp-name (concat (buffer-file-name) "_"
+                                                    (format-time-string "%Y%m%d_%H%M%S_")) ) ".png"))
+           (wslfilename (string-trim (shell-command-to-string (concat "wslpath -m \"$(realpath '" filename "')\""))))
+           (wslshortfile (format "./%s" (file-name-nondirectory wslfilename))))
+           ; (message (format "1|filename=%s|fullpath=%s|shortname=%s" filename wslfilename wslshortfile))
+           ; (shell-command "snippingtool.exe /clip")
+           (shell-command (concat "powershell.exe -command 'Add-Type -AssemblyName System.Windows.Forms;if ($([System.Windows.Forms.Clipboard]::ContainsImage())) {$image = [System.Windows.Forms.Clipboard]::GetImage();[System.Drawing.Bitmap]$image.Save(\""wslfilename "\",[System.Drawing.Imaging.ImageFormat]::Png); Write-Output \"clipboard content saved as file\"} else {Write-Output \"clipboard does not contain image data\"}'"))       
+           (insert (concat "[[file:"wslshortfile"]]"))
+      )
+    )
+   )
   (org-display-inline-images))
 
+(setq filename "/home/verslyfr/winhome/has a space/foo bar.c")
+(string-trim (shell-command-to-string (concat "wslpath -m \"$(realpath '"filename"')\"")))
+
+
+
+ssss
